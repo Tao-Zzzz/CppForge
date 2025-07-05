93a0db9b856f963e53c04a3fc98f8a3a5937f008
L-Last-Lost <2194774308@qq.com>
2025-07-05 23:51:06 +0800

    ThreadPool

---
+++ Pool/ThreadPool.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+
+class ThreadPool{
+public:
+    explicit ThreadPool(size_t thread_count) : stop(false){
+        for(size_t i = 0; i < thread_count; ++i){
+            workers.emplace_back([this](){
+                while(true){
+                    std::function<void()> task;
+
+                    {
+                        std::unique_lock<std::mutex> lock(mtx);
+                        
+                        cv.wait(lock, [this](){
+                            return stop || !tasks.empty();
+                        });
+
+                        if(stop && tasks.empty()){
+                            return;
+                        }
+
+                        task = std::move(tasks.front());
+                        tasks.pop();
+                    }
+
+                    task();
+                }
+            });
+        }
+    }
+
+    // 用move来获得任务..
+    void enqueue(std::function<void()> task){
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            tasks.push(std::move(task));
+        }
+    }
+
+    ~ThreadPool(){
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            stop = true;
+        }
+
+        cv.notify_all();
+        // 线程销毁..
+        for(auto& t : workers){
+            t.join();
+        }
+    }
+
+
+private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+
+    std::mutex mtx;
+    std::condition_variable cv;
+    bool stop;
+};
