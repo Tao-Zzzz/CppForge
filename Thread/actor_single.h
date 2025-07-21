ec5814a22fe18d637b582a7a4a7abc7e22c431e5
L-Last-Lost <2194774308@qq.com>
2025-07-21 11:27:35 +0800

    CRTP

---
+++ Thread/actor_single.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <thread>
+#include "thread_safe_queue.h"
+#include <atomic>
+#include <iostream>
+#include <memory>
+
+template <typename ClassType, typename QueType>
+class ActorSingle
+{
+public:
+    static ClassType &Inst()
+    {
+        static ClassType as;
+        return as;
+    }
+
+    ~ActorSingle()
+    {
+    }
+
+    void PostMsg(const QueType &data)
+    {
+        _que.push(data);
+    }
+
+protected:
+    ActorSingle() : _bstop(false)
+    {
+    }
+
+    ActorSingle(const ActorSingle &) = delete;
+    ActorSingle(ActorSingle &&) = delete;
+    ActorSingle &operator=(const ActorSingle &) = delete;
+
+    std::atomic<bool> _bstop;
+    ThreadSafeQue<QueType> _que;
+    std::thread _thread;
+};
